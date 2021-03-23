feecb2e145c6567355c41f9024ec66ebc76b3471
Xerxes <xerxestarman@gmail.com>
2021-03-23 14:31:42 -0500

    F

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,11 @@ const Express = require('express');
 
 const app = new Express();
 
+const path = require('path');
+// Use Joi  
+
+app.use(Express.json());
+
 // 1 = black 
 // 2 = red
 // 0 = blank
@@ -18,34 +23,64 @@ const app = new Express();
  }
 
 app.get("/newGame",(req,res)=>{
-    // set board values to zero;
-    res.send(board);
+    
+    res.send("CyCheckers"+"\n"+Printbord());
+});
+
+
+//{
+//    "color": "b",
+//    "currPos": [0,4],
+//    "toPos": [5,3]
+//}
+
+
+app.get("/game",(req,res)=>{
+    res.sendFile(path.join(__dirname,'Client','Checkers.html'));
+});
+
+app.get("/leaderboard",(req,res)=>{
+    res.sendFile(path.join(__dirname,'Client','Leaderboard','leaderboard.html'));
 });
 
 
 //black or red ,curPos  , toPos
-app.get("/:color/:currX/:currY/:toPosx/:toPosY",(req,res)=>{
+app.post("/move",(req,res)=>{
 
-    color = req.params.color
+    
 
-    currPos = [req.params.currx,req.params.currY]
+    const movingPiece = req.body;
 
-    toPos = [req.params.toPosx,req.params.toPosY]
+    if (checkVal(movingPiece) == true){
+        color = 0
+        if (movingPiece.color == "b"){color = 1}
+        if(movingPiece.color == "r"){color = 2}
+        board['row'+movingPiece.toPos[1]][movingPiece.toPos[0]] = color;
+        board['row'+movingPiece.currPos[1]][movingPiece.currPos[0]] = 0;
 
-    if (checkVal(currPos,toPos) == true){
-        board['row'+toPos[0]][toPos[1]] = color;
-        board['row'+currPos[0]][currPos[1]] = 0;
+
     }
-
-    res.send(board);
+    res.send(Printbord());
 });
 
-
+function Printbord(){
+    let stringBoard = JSON.stringify(board.row0)+'\n';
+    stringBoard += JSON.stringify(board.row1)+'\n';
+    stringBoard += JSON.stringify(board.row2)+'\n';
+    stringBoard += JSON.stringify(board.row3)+'\n';
+    stringBoard += JSON.stringify(board.row4)+'\n';
+    stringBoard += JSON.stringify(board.row5)+'\n';
+    stringBoard += JSON.stringify(board.row6)+'\n';
+    stringBoard += JSON.stringify(board.row7)+'\n';
+    console.log(stringBoard);
+    return stringBoard;
+}
 // TODO
-//function checkVal(currpos,pos){
-  //  return true;
-    //return false;
-//}
+function checkVal(movingPiece){
+
+
+  return true;
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`Listing to port ${port}`));
